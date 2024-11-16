edbec2126f7bbb02a54cb1912c8e527223f52e67
Timothy <oludefiyinfoluwa06@gmail.com>
2024-11-16 10:32:29 +0100

    Implement email messaging, make updates on frontend

---
+++ server/controllers/email.controller.js
@@ -0,0 +1,43 @@
+const nodemailer = require('nodemailer');
+
+const sendMessage = async (req, res) => {
+    const { name, email, subject, message } = req.body;
+
+    try {
+        const emailBody = `
+            <h1>${name}</h1>
+            <p>${email}</p>
+            <p>${message}</p>
+        `;
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        });
+
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: 'oludefiyinfoluwa06@gmail.com',
+            subject: subject || `Message from ${name}`,
+            html: emailBody
+        }
+
+        await transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                return res.json({ 'error': 'An error occured' });
+            }
+
+            return res.json({ 'message': 'Email sent successfully' });
+        });
+    } catch (error) {
+        console.log(error);
+        return res.json({ 'error': error.message });
+    }
+};
+
+module.exports = {
+    sendMessage,
+}
--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const propertiesRoute = require('./routes/property.route');
 const reviewsRoute = require('./routes/review.route');
 const chatRoute = require('./routes/chat.route');
 const paymentRoute = require('./routes/payment.route');
+const emailRoute = require('./routes/email.route');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -40,11 +41,13 @@ mongoose.connect(process.env.DB_URI)
     })
     .catch(err => console.log(err, 'Connection unsuccessful'));
 
+app.get('/', (req, res) => res.json('Hello world'));
 app.use('/api/user', usersRoute);
 app.use('/api/property', propertiesRoute);
 app.use('/api/reviews', reviewsRoute);
 app.use('/api/chat', chatRoute);
 app.use('/api/payment', paymentRoute);
+app.use('/api/email', emailRoute);
 
 
 io.on('connection', (socket) => {
